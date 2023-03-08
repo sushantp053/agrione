fd23a4c0209eac35f41ca16bd338a1c557cd1bcb
Sushant <sushantp053@gmail.com>
2023-03-08 10:32:22 +0530

    Login and splash screen added

---
+++ app/src/main/java/com/macmads/agrione/VerifyOTP.kt
@@ -0,0 +1,115 @@
+package com.macmads.agrione
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.macmads.agrione.databinding.ActivityVerifyOtpBinding
+import java.util.concurrent.TimeUnit
+
+class VerifyOTP : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var verificationId: String
+    private lateinit var dialog: AlertDialog
+    private val db = Firebase.firestore
+
+    private lateinit var binding: ActivityVerifyOtpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityVerifyOtpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = Firebase.auth
+
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Please wait.....")
+        builder.setTitle("Loading")
+        builder.setCancelable(false)
+
+        dialog = builder.create()
+        dialog.show()
+
+        val phoneNumber = "+91" + intent.getStringExtra("number")
+
+        binding.otpMobile.text = phoneNumber
+
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                }
+
+                override fun onVerificationFailed(p0: FirebaseException) {
+                    dialog.dismiss()
+                    Toast.makeText(this@VerifyOTP, "Please Try Again", Toast.LENGTH_SHORT).show()
+
+                }
+
+                override fun onCodeSent(p0: String, p1: PhoneAuthProvider.ForceResendingToken) {
+                    super.onCodeSent(p0, p1)
+                    dialog.dismiss()
+                    verificationId = p0
+                }
+            }).build()
+
+        PhoneAuthProvider.verifyPhoneNumber(options)
+
+        binding.verifyOTPCard.setOnClickListener {
+            if (binding.editTextOtp.text!!.isEmpty()) {
+                Toast.makeText(this, "Please Enter OTP....", Toast.LENGTH_SHORT).show()
+            } else {
+                dialog.show()
+                val credential =
+                    PhoneAuthProvider.getCredential(verificationId, binding.editTextOtp.text!!.toString())
+                auth.signInWithCredential(credential).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val docRef = db.collection("users").document(auth.uid.toString())
+                        docRef.get()
+                            .addOnSuccessListener { document ->
+                                if (document.data != null) {
+                                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                                    startActivity(Intent(this, MainActivity::class.java))
+                                    finish()
+                                } else {
+                                    Log.d(TAG, "No such document")
+//                                    RegisterUser
+//                                    startActivity(Intent(this, RegisterUser::class.java))
+//                                    finish()
+                                }
+                            }
+                            .addOnFailureListener { exception ->
+                                Log.d(TAG, "get failed with ", exception)
+                            }
+                        dialog.dismiss()
+
+                    } else {
+                        dialog.dismiss()
+                        Toast.makeText(this@VerifyOTP, "Error ${it.exception}", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+        }
+        binding.changeMobile.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        }
+    }
+}
