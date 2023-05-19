17f184979675bf088b5fe01079d8f91dca0fd247
sushant windows <sushantp053@gmail.com>
2023-05-19 07:52:57 +0530

    feat - vision library added

--- app/src/main/java/com/macmads/agrione/MainActivity.kt
+++ app/src/main/java/com/macmads/agrione/MainActivity.kt
@@ -16,6 +16,9 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import com.macmads.agrione.databinding.ActivityMainBinding
 import java.io.IOException
 
@@ -31,6 +34,7 @@ class MainActivity : AppCompatActivity() {
     private val mModelPath = "plant_disease_model.tflite"
     private val mLabelPath = "plant_labels.txt"
     private val mSamplePath = "soybean.JPG"
+    private var detection = false
 
     private lateinit var binding: ActivityMainBinding
 
@@ -64,8 +68,9 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(callGalleryIntent, mGalleryRequestCode)
         }
         binding.mDetectButton.setOnClickListener {
-            val results = mClassifier.recognizeImage(mBitmap).firstOrNull()
-            binding.mResultTextView.text= results?.title+"\n Confidence:"+results?.confidence
+            processImage()
+//            val results = mClassifier.recognizeImage(mBitmap).firstOrNull()
+//            binding.mResultTextView.text= results?.title+"\n Confidence:"+results?.confidence
 
         }
         binding.logoutCard.setOnClickListener {
@@ -90,22 +95,75 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun processImage() {
+
+        val imageLabeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
+        val inputImage = InputImage.fromBitmap(mBitmap!!, 0)
+        imageLabeler.process(inputImage).addOnSuccessListener { imageLabels ->
+            var stringImageRecognition = ""
+            for (imageLabel in imageLabels) {
+                val stingLabel = imageLabel.text
+                val floatConfidence = imageLabel.confidence
+                val index = imageLabel.index
+                stringImageRecognition += "$index\n $stingLabel:\n $floatConfidence "
+                if (index == 266 && floatConfidence > 0.7) {
+                    detection = true
+                }
+            }
+            if (detection) {
+                val results = mClassifier.recognizeImage(mBitmap).firstOrNull()
+                binding.mResultTextView.text =
+                    results?.title + "\n Confidence:" + results?.confidence
+                detection = false
+            } else {
+                binding.mResultTextView.text = stringImageRecognition
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Not a plant")
+                builder.setMessage("Given image is invalid steal do you want to continue to check disease")
+                builder.setPositiveButton("Detect") { dialog, which ->
+
+                    val results = mClassifier.recognizeImage(mBitmap).firstOrNull()
+                    binding.mResultTextView.text =
+                        results?.title + "\n Confidence:" + results?.confidence
+                    dialog.dismiss()
+
+                }
+                builder.setNegativeButton(android.R.string.no) { dialog, which ->
+                    Toast.makeText(
+                        applicationContext,
+                        "Other object detected", Toast.LENGTH_SHORT
+                    ).show()
+                    binding.mResultTextView.text = stringImageRecognition
+                    dialog.dismiss()
+                }
+                builder.show()
+                detection = false
+            }
+        }
+
+
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == mCameraRequestCode){
+        if (requestCode == mCameraRequestCode) {
             //Considérons le cas de la caméra annulée
-            if(resultCode == Activity.RESULT_OK && data != null) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
                 mBitmap = data.extras!!.get("data") as Bitmap
                 mBitmap = scaleImage(mBitmap)
-                val toast = Toast.makeText(this, ("Image crop to: w= ${mBitmap.width} h= ${mBitmap.height}"), Toast.LENGTH_LONG)
+                val toast = Toast.makeText(
+                    this,
+                    ("Image crop to: w= ${mBitmap.width} h= ${mBitmap.height}"),
+                    Toast.LENGTH_LONG
+                )
                 toast.setGravity(Gravity.BOTTOM, 0, 20)
                 toast.show()
                 binding.mPhotoImageView.setImageBitmap(mBitmap)
-                binding.mResultTextView.text= "Your photo image set now."
+                binding.mResultTextView.text = "Your photo image set now."
             } else {
                 Toast.makeText(this, "Camera cancel..", Toast.LENGTH_LONG).show()
             }
-        } else if(requestCode == mGalleryRequestCode) {
+        } else if (requestCode == mGalleryRequestCode) {
             if (data != null) {
                 val uri = data.data
 
